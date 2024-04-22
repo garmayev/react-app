021fc982cdfb882e6defe185b23899d44be98f4c
garmayev <garmayev@yandex.ru>
2024-04-22 14:48:53 +0800

    Drawer init

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+const HomeScreen = () => {
+    console.log("Home?")
+    return (
+        <View>
+            <Text>This is Home Screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
